5cfbbc494e1c7a42f931355da45e29b2c095e0af
Vsevolod Popov <sevapopov13@gmail.com>
2021-11-21 22:00:46 +0300

    Failure file with the failure message is added.

---
+++ Failure.php
@@ -0,0 +1,14 @@
+<?php
+include 'functions.php';
+?>
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>Ошибка></title>
+    </head>
+    <body>
+        <h1>Ошибка></h1>
+        <p>К сожалению, вы ошиблись. А право на ошибку даётся только 1 раз... Но ничего, перезапустите программу снова и начните сначала!</p>
+    </body>
+</html>
